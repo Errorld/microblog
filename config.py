c78498f7a4f81a53d7dbb52ce6147c61137c7ba6
Errorld <errorld@outlook.com>
2015-10-17 19:11:06 -0700

    to 02 websheet

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 CSRF_ENABBED = True
 SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
+]
