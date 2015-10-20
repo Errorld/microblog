cbc132f32761055ba02911866386c8a129d869b4
Errorld <errorld@outlook.com>
2015-10-20 08:46:00 -0700

    3

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# SQLALCHEMY
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+# CSRF PROTECT
 CSRF_ENABBED = True
 SECRET_KEY = 'you-will-never-guess'
 
+# OPENID
 OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
